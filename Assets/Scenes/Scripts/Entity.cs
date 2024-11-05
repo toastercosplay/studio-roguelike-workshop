36d1a6b791b150b289afc1b586f7c7c3bde93ff9
toastercosplay <andrewdouglas2005@gmail.com>
2024-11-04 18:52:58 -0800

    :3

--- Assets/Scenes/Scripts/Entity.cs
+++ Assets/Scenes/Scripts/Entity.cs
@@ -10,7 +10,34 @@ public class Entity : MonoBehaviour
     public struct EntityStats
     {
         public float MovementSpeed;
+        public float MaxHealth;
     }
 
     public EntityStats stats;
+
+    [NonSerialized]
+
+    public float CurrentHealth;
+    bool dead = false;
+
+    private void Start()
+    {
+        CurrentHealth = stats.MaxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHealth -= Mathf.Ceil(damage);
+
+        if (CurrentHealth <= 0 )
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        dead = true;
+        Destroy(gameObject);
+    }
 }
